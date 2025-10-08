466e4c727e6442709d78b8cdcc6508306fd6f83f
one030914 <one030914@gmail.com>
2025-10-08 13:29:21 +0800

    hw update

--- 03/app/src/main/java/com/ehappy/exmultibutton/MainActivity.java
+++ 03/app/src/main/java/com/ehappy/exmultibutton/MainActivity.java
@@ -9,17 +9,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // 宣告全域變數
-    private TextView txtShow;
+    private TextView result, displayOperation;
     private Button btnZero,btnOne,btnTwo,btnThree,btnFour;
     private Button btnFive,btnSix,btnSeven,btnEight,btnNine;
     private Button plus, sub, mul, div, clear, sum, del, not, sqr, sqrt, left, right;
-    private double result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 取得資源類別檔中的介面元件
-        txtShow=(TextView)findViewById(R.id.txtShow);
+        result=(TextView)findViewById(R.id.result);
+        displayOperation=(TextView)findViewById(R.id.displayOperation);
+
         btnZero=(Button)findViewById(R.id.btnZero);
         btnOne=(Button)findViewById(R.id.btnOne);
         btnTwo=(Button)findViewById(R.id.btnTwo);
@@ -30,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         btnSeven=(Button)findViewById(R.id.btnSeven);
         btnEight=(Button)findViewById(R.id.btnEight);
         btnNine=(Button)findViewById(R.id.btnNine);
-
 
         plus=(Button)findViewById(R.id.plus);
         sub=(Button)findViewById(R.id.sub);
@@ -45,129 +46,36 @@ public class MainActivity extends AppCompatActivity {
         left=(Button)findViewById(R.id.left);
         right=(Button)findViewById(R.id.right);
 
-        // 設定 button 元件 Click 事件共用   myListener
-        btnZero.setOnClickListener(myListener);
-        btnOne.setOnClickListener(myListener);
-        btnTwo.setOnClickListener(myListener);
-        btnThree.setOnClickListener(myListener);
-        btnFour.setOnClickListener(myListener);
-        btnFive.setOnClickListener(myListener);
-        btnSix.setOnClickListener(myListener);
-        btnSeven.setOnClickListener(myListener);
-        btnEight.setOnClickListener(myListener);
-        btnNine.setOnClickListener(myListener);
+        btnZero.setOnClickListener(number);
+        btnOne.setOnClickListener(number);
+        btnTwo.setOnClickListener(number);
+        btnThree.setOnClickListener(number);
+        btnFour.setOnClickListener(number);
+        btnFive.setOnClickListener(number);
+        btnSix.setOnClickListener(number);
+        btnSeven.setOnClickListener(number);
+        btnEight.setOnClickListener(number);
+        btnNine.setOnClickListener(number);
 
-        plus.setOnClickListener(myListener);
-        sub.setOnClickListener(myListener);
-        mul.setOnClickListener(myListener);
-        div.setOnClickListener(myListener);
-        clear.setOnClickListener(myListener);
-        sum.setOnClickListener(myListener);
-        del.setOnClickListener(myListener);
-        not.setOnClickListener(myListener);
-        sqr.setOnClickListener(myListener);
-        sqrt.setOnClickListener(myListener);
-        left.setOnClickListener(myListener);
-        right.setOnClickListener(myListener);
+//        plus.setOnClickListener(myListener);
+//        sub.setOnClickListener(myListener);
+//        mul.setOnClickListener(myListener);
+//        div.setOnClickListener(myListener);
+//        clear.setOnClickListener(myListener);
+//        sum.setOnClickListener(myListener);
+//        del.setOnClickListener(myListener);
+//        not.setOnClickListener(myListener);
+//        sqr.setOnClickListener(myListener);
+//        sqrt.setOnClickListener(myListener);
+//        left.setOnClickListener(myListener);
+//        right.setOnClickListener(myListener);
     }
 
-    // 定義  onClick() 方法
-    private Button.OnClickListener myListener=new Button.OnClickListener(){
+    private Button.OnClickListener number = new Button.OnClickListener(){
         @Override
-        public void onClick(View v) {
-            String s=txtShow.getText().toString();
-            switch (v.getId()) {
-                case R.id.btnZero: {
-                    txtShow.setText(s + "0");
-                    break;
-                }
-                case R.id.btnOne: {
-                    txtShow.setText(s + "1");
-                    break;
-                }
-                case R.id.btnTwo:{
-                    txtShow.setText(s + "2");
-                    break;
-                }
-                case R.id.btnThree:{
-                    txtShow.setText(s + "3");
-                    break;
-                }
-                case R.id.btnFour:{
-                    txtShow.setText(s + "4");
-                    break;
-                }
-                case R.id.btnFive:{
-                    txtShow.setText(s + "5");
-                    break;
-                }
-                case R.id.btnSix:{
-                    txtShow.setText(s + "6");
-                    break;
-                }
-                case R.id.btnSeven:{
-                    txtShow.setText(s + "7");
-                    break;
-                }
-                case R.id.btnEight:{
-                    txtShow.setText(s + "8");
-                    break;
-                }
-                case R.id.btnNine:{
-                    txtShow.setText(s + "9");
-                    break;
-                }
-                case R.id.clear:{
-                    txtShow.setText("");
-                    break;
-                }
-                case R.id.sum:{
-                    break;
-                }
-                case R.id.plus:{
-                    txtShow.setText(s + "+");
-                    break;
-                }
-                case R.id.sub:{
-                    txtShow.setText(s + "-");
-                    break;
-                }
-                case R.id.mul:{
-                    txtShow.setText(s + "*");
-                    break;
-                }
-                case R.id.div:{
-                    txtShow.setText(s + "/");
-                    break;
-                }
-                case R.id.del:{
-                    if(s.length()>0) {
-                        s=s.substring(0, s.length()-1);
-                        txtShow.setText(s);
-                    }
-                    break;
-                }
-//                case R.id.not:{
-//
-//                    break;
-//                }
-                case R.id.sqr:{
-                    txtShow.setText(s + "^2");
-                    break;
-                }
-                case R.id.sqrt:{
-                    txtShow.setText("√" + s);
-                    break;
-                }
-                case R.id.left:{
-                    txtShow.setText(s + "(");
-                    break;
-                }
-                case R.id.right:{
-                    txtShow.setText(s + ")");
-                    break;
-                }
-            }
+        public void onClick(View v){
+            Button b = (Button) v;
+            result.append(b.getText().toString());
         }
     };
 }
---
+++ 04/app/src/main/java/com/ehappy/exradiobutton02/MainActivity.java
@@ -0,0 +1,107 @@
+package com.ehappy.exradiobutton02;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtResult;
+    private RadioGroup dish, drink, serving;
+    private RadioButton pork, chicken, rice;
+    private RadioButton juice, blacktea, greentea;
+    private RadioButton one, two, three;
+    private Button submit;
+    private String str, s1, s2, s3;
+    private int summary, p1, p2, p3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 取得介面元件 id
+        txtResult=(TextView) findViewById(R.id.txtResult);
+        submit = (Button) findViewById(R.id.submit);
+
+        dish = (RadioGroup) findViewById(R.id.dish);
+        drink = (RadioGroup) findViewById(R.id.drink);
+        serving = (RadioGroup) findViewById(R.id.serving);
+
+        pork = (RadioButton) findViewById(R.id.pork);
+        chicken = (RadioButton) findViewById(R.id.chicken);
+        rice = (RadioButton) findViewById(R.id.rice);
+
+        juice = (RadioButton) findViewById(R.id.juice);
+        blacktea = (RadioButton) findViewById(R.id.blacktea);
+        greentea = (RadioButton) findViewById(R.id.greentea);
+
+        one = (RadioButton) findViewById(R.id.one);
+        two = (RadioButton) findViewById(R.id.two);
+        three = (RadioButton) findViewById(R.id.three);
+
+        dish.setOnCheckedChangeListener(main_dish);
+        drink.setOnCheckedChangeListener(Drink);
+        serving.setOnCheckedChangeListener(Serving);
+        submit.setOnClickListener(Submit);
+    }
+
+    private RadioGroup.OnCheckedChangeListener main_dish = new RadioGroup.OnCheckedChangeListener(){
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId == R.id.pork){
+                s1 = "豬排飯";
+                p1 = 120;
+            }else if(checkedId == R.id.chicken){
+                s1 = "雞腿飯";
+                p1 = 150;
+            }else if(checkedId == R.id.rice){
+                s1 = "炒飯";
+                p1 = 65;
+            }
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener Drink = new RadioGroup.OnCheckedChangeListener(){
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId == R.id.juice){
+                s2 = "果汁";
+                p2 = 20;
+            }else if(checkedId == R.id.blacktea){
+                s2 = "紅茶";
+                p2 = 15;
+            }else if(checkedId == R.id.greentea){
+                s2 = "綠茶";
+                p2 = 10;
+            }
+        }
+    };
+
+    private RadioGroup.OnCheckedChangeListener Serving = new RadioGroup.OnCheckedChangeListener(){
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            if (checkedId == R.id.one){
+                s3 = "一份";
+                p3 = 1;
+            }else if(checkedId == R.id.two){
+                s3 = "兩份";
+                p3 = 2;
+            }else if(checkedId == R.id.three){
+                s3 = "三份";
+                p3 = 3;
+            }
+        }
+    };
+
+    private Button.OnClickListener Submit = new Button.OnClickListener(){
+        public void onClick(View v){
+            summary = (p1 + p2) * p3;
+            str = "你選擇 " + s1 + ", " + s2 + ", " + s3 + ", 共 " + summary + " 元";
+            txtResult.setText(str);
+        }
+    };
+}
